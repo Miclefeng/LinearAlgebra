4067613264ea9ce1a16ed1ab73aaeba06a250d0a
miclefeng <miclefengzss@gmail.com>
2018-11-07 11:33:43 +0800

    edit

--- src/LinearAlgebra/vectorT/vectorT.go
+++ src/LinearAlgebra/vectorT/vectorT.go
@@ -6,13 +6,20 @@ import (
 )
 
 func main() {
-	va := vector.Vectors{4, 3, 2}
+	va := vector.Vectors{4, 6, 2}
 	vb := vector.Vectors{5, 2, 1}
 
-	newVec, err := vector.Add(va, vb)
+	addVec, err := vector.Add(va, vb)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	fmt.Printf("(%v) + (%v) = (%v)", va, vb, newVec)
+	a, b := 3, 2
+	fmt.Printf("(%v) + (%v) = (%v)\n", va, vb, addVec)
+	subVec, _ := vector.Sub(va, vb)
+	fmt.Printf("(%v) - (%v) = (%v)\n", va, vb, subVec)
+	mulVec := vector.Mul(a, vb)
+	fmt.Printf("(%v) * %d = (%v)\n", va, a, mulVec)
+	divVec := vector.Div(b, va)
+	fmt.Printf("(%v) / %d = (%v)\n", va, b, divVec)
 }
